ace328d3b78e41d33578f9456baa3f0f5e840c81
Hernandez , Edgar (Contractor) <ehernandez1@k12.com>
2022-09-27 14:47:47 -0300

    Improved logs and parameterized URL for API consumption

--- src/main/java/com/dreamseeker/fetchusdclp/Handler.java
+++ src/main/java/com/dreamseeker/fetchusdclp/Handler.java
@@ -12,28 +12,35 @@ import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
 public class Handler implements RequestHandler<Map<String, Object>, String> {
-
+	// TODO automate lambda upload to aws, use Serverless framework
+	// TODO unit test, find the way to test locally
 	private static final HttpClient httpClient = HttpClient.newBuilder().connectTimeout(Duration.ofSeconds(10)).build();
 
 	@Override
 	public String handleRequest(Map<String, Object> event, Context context) {
 		LambdaLogger logger = context.getLogger();
-		// TODO parameterized baseUrl, endpoint path, symbols
-		// Use env vars for this
-		URI uri = URI.create(
-				"https://openexchangerates.org/api/latest.json?app_id=833c87e7e2ca49baabf0efce616c5826&symbols=CLP");
+		Map<String, String> envVars = System.getenv();
+		String baseUrl = envVars.get("BASE_URL");
+		String endpointPath = envVars.get("ENDPOINT_PATH");
+		String appId = envVars.get("APP_ID");
+		String symbols = envVars.get("SYMBOLS");
+		StringBuilder stringBuilder = new StringBuilder();
+		String completeUrl = stringBuilder.append(baseUrl).append(endpointPath).append("?").append("app_id=").append(appId).append("&")
+				.append("symbols=").append(symbols).toString();
+		logger.log("Request URL -> " + completeUrl);
+		URI uri = URI.create(completeUrl);
 		HttpRequest request = HttpRequest.newBuilder().GET().uri(uri).build();
-		String response = "No response";
+		String responseBody = "No response";
 		try {
 			// TODO create a response type
-			java.net.http.HttpResponse<String> httpResponse = httpClient.send(request,
-					java.net.http.HttpResponse.BodyHandlers.ofString());
-			logger.log("Response: " + httpResponse.body() + " | status code: " + httpResponse.statusCode());
+			java.net.http.HttpResponse<String> httpResponse = httpClient.send(request, java.net.http.HttpResponse.BodyHandlers.ofString());
+			responseBody = httpResponse.body();
+			logger.log("Response: " + responseBody + " | status code: " + httpResponse.statusCode());
 		} catch (IOException | InterruptedException e) {
 			logger.log(e.getMessage());
 		}
 
-		return response;
+		return responseBody;
 	}
 
 }
